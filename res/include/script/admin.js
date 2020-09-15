2176d93ae8debe083e6e0b369e7cd430a460b8a2
Wim Brand <fornax@fornaxian.com>
2020-09-15 15:02:20 +0200

    update colors

--- res/include/script/admin.js
+++ res/include/script/admin.js
@@ -214,9 +214,9 @@ function getStats(order) {
 			<td>${formatDataVolume(v.free_space, 3)}</td>
 			<td>${formatDataVolume(v.min_free_space, 3)}</td>`
 
-			if (v.free_space < v.min_free_space) {
+			if (v.free_space < v.min_free_space/2) {
 				row.classList.add("highlight_red")
-			} else if (v.free_space < v.min_free_space*2) {
+			} else if (v.free_space < v.min_free_space) {
 				row.classList.add("highlight_blue")
 			} else {
 				row.classList.add("highlight_green")
