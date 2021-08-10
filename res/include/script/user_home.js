a7bf875dcc685b406ecae876acd1b185df6ac45c
Wim Brand <fornax@fornaxian.com>
2021-08-10 19:17:54 +0200

    Add direct linking to user home

--- res/include/script/user_home.js
+++ res/include/script/user_home.js
@@ -1,24 +1,24 @@
-function loadGraph(graph, stat, minutes, interval){
+function loadGraph(graph, stat, minutes, interval) {
 	let today = new Date()
 	let start = new Date()
-	start.setMinutes(start.getMinutes()-minutes)
+	start.setMinutes(start.getMinutes() - minutes)
 
 	fetch(
-		apiEndpoint+"/user/time_series/" + stat +
-		"?start="+start.toISOString() +
-		"&end="+today.toISOString() +
-		"&interval="+interval
+		apiEndpoint + "/user/time_series/" + stat +
+		"?start=" + start.toISOString() +
+		"&end=" + today.toISOString() +
+		"&interval=" + interval
 	).then(resp => {
-		if (!resp.ok) { return Promise.reject("Error: "+resp.status);}
+		if (!resp.ok) { return Promise.reject("Error: " + resp.status); }
 		return resp.json();
 	}).then(resp => {
 		resp.timestamps.forEach((val, idx) => {
 			let date = new Date(val);
-			let dateStr = ("00"+(date.getMonth()+1)).slice(-2);
-			dateStr += "-"+("00"+date.getDate()).slice(-2);
-			dateStr += " "+("00"+date.getHours()).slice(-2);
-			dateStr += ":"+("00"+date.getMinutes()).slice(-2);
-			resp.timestamps[idx] = "   "+dateStr+"   "; // Poor man's padding
+			let dateStr = ("00" + (date.getMonth() + 1)).slice(-2);
+			dateStr += "-" + ("00" + date.getDate()).slice(-2);
+			dateStr += " " + ("00" + date.getHours()).slice(-2);
+			dateStr += ":" + ("00" + date.getMinutes()).slice(-2);
+			resp.timestamps[idx] = "   " + dateStr + "   "; // Poor man's padding
 		});
 		graph.data.labels = resp.timestamps;
 		graph.data.datasets[0].data = resp.amounts;
@@ -35,27 +35,53 @@ function loadGraph(graph, stat, minutes, interval){
 			document.getElementById("total_downloads").innerText = formatThousands(total);
 		} else if (stat == "bandwidth") {
 			document.getElementById("total_bandwidth").innerText = formatDataVolume(total, 3);
+		} else if (stat == "direct_bandwidth") {
+			document.getElementById("total_direct_bandwidth").innerText = formatDataVolume(total, 3);
 		}
 	}).catch(e => {
-		console.error("Error requesting time series: "+e);
+		console.error("Error requesting time series: " + e);
+	})
+}
+
+function loadDirectBW() {
+	let today = new Date()
+	let start = new Date()
+	start.setDate(start.getDate() - 30)
+
+	fetch(
+		apiEndpoint + "/user/time_series/direct_bandwidth" +
+		"?start=" + start.toISOString() +
+		"&end=" + today.toISOString() +
+		"&interval=60"
+	).then(resp => {
+		if (!resp.ok) { return Promise.reject("Error: " + resp.status); }
+		return resp.json();
+	}).then(resp => {
+		let total = resp.amounts.reduce((accum, val) => accum += val, 0);
+		document.getElementById("direct_bandwidth_month").innerText = formatDataVolume(total, 4)
+	}).catch(e => {
+		console.error("Error requesting time series: " + e);
 	})
 }
 
 let graphViews = drawGraph(document.getElementById("views_chart"), "Views", "number");
 let graphDownloads = drawGraph(document.getElementById("downloads_chart"), "Downloads", "number");
 let graphBandwidth = drawGraph(document.getElementById("bandwidth_chart"), "Bandwidth", "bytes");
+let graphDirectBandwidth = drawGraph(document.getElementById("direct_bandwidth_chart"), "Direct Bandwidth", "bytes");
 let graphTimeout = null;
 
 function updateGraphs(minutes, interval, live) {
 	if (graphTimeout !== null) { clearTimeout(graphTimeout) }
 	if (live) {
-		graphTimeout = setTimeout(() => {updateGraphs(minutes, interval, true)}, 10000)
+		graphTimeout = setTimeout(() => { updateGraphs(minutes, interval, true) }, 10000)
 	}
 
 	loadGraph(graphViews, "views", minutes, interval);
 	loadGraph(graphDownloads, "downloads", minutes, interval);
 	loadGraph(graphBandwidth, "bandwidth", minutes, interval);
+	loadGraph(graphDirectBandwidth, "direct_bandwidth", minutes, interval);
+	loadDirectBW()
 }
 
 // Default
-updateGraphs(10080, 60, false);
+updateGraphs(1440, 1, true);
--- webcontroller/file_viewer.go
+++ webcontroller/file_viewer.go
@@ -78,22 +78,18 @@ func (vd *viewerData) adType(files []pixelapi.ListFile) {
 	// Intn returns a number up to n, but never n itself. So to get a random 0
 	// or 1 we need to give it n=2. We can use this function to make other
 	// splits like 1/3 1/4, etc
-	switch i := rand.Intn(8); i {
-	case 0: // 12.5%
+	switch i := rand.Intn(4); i {
+	case 0: // 25%
 		vd.AdBannerType = brave
-	case 1, 2, 3, 4, 5, 6, 7: // 87.5%
+	case 2, 3, 4: // 75%
 		vd.AdBannerType = aAds
 	default:
 		panic(fmt.Errorf("random number generator returned unrecognised number: %d", i))
 	}
 
-	// If the file is larger than 10 MB we enable floating popups
-	if avgSize > 10e6 {
+	// If the file is larger than 5 MB we enable floating popups
+	if avgSize > 5e6 {
 		vd.AdFloaterType = propellerFloat
-	}
-
-	if avgSize > 250e6 {
-		vd.AdPopupType = clickAduPopup
 	}
 }
 
