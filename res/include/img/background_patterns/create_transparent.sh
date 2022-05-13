1f54037f62ad6dda6bc87ac0c8fd0876133f59ea
Fornax <wim@fornaxian.tech>
2022-05-13 17:43:55 +0200

    Make backgrounds slightly more transparent

--- res/include/img/background_patterns/create_transparent.sh
+++ res/include/img/background_patterns/create_transparent.sh
@@ -4,12 +4,12 @@ transparent () {
 	convert $1.png -alpha set -channel a -evaluate set $2 $1_transparent.png
 }
 
-for i in {0..17}; do transparent "checker${i}" 6%; done
-transparent "checker_wednesday" 10%
-transparent "checker_dwarf" 6%
-transparent "checker_developers" 6%
-transparent "checker_halloween" 6%
-transparent "checker_christmas" 6%
-transparent "checker_ukraine" 10%
+for i in {0..17}; do transparent "checker${i}" 5%; done
+transparent "checker_wednesday" 8%
+transparent "checker_dwarf" 5%
+transparent "checker_developers" 5%
+transparent "checker_halloween" 5%
+transparent "checker_christmas" 5%
+transparent "checker_ukraine" 8%
 
 optipng *.png
