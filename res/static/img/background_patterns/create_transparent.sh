1eb1e883926189a378de1a949b0e0ba10d78afab
Fornax <wim@fornaxian.tech>
2022-08-08 14:03:10 +0200

    Increase visibility for thematic backgrounds

--- res/static/img/background_patterns/create_transparent.sh
+++ res/static/img/background_patterns/create_transparent.sh
@@ -6,10 +6,10 @@ transparent () {
 
 for i in {0..17}; do transparent "checker${i}" 5%; done
 transparent "checker_wednesday" 8%
-transparent "checker_dwarf" 5%
-transparent "checker_developers" 5%
-transparent "checker_halloween" 5%
-transparent "checker_christmas" 5%
+transparent "checker_dwarf" 8%
+transparent "checker_developers" 8%
+transparent "checker_halloween" 8%
+transparent "checker_christmas" 8%
 transparent "checker_ukraine" 8%
 
 optipng *.png
