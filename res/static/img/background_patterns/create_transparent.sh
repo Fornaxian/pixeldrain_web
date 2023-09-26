8c94c04b2216609fdf50bf62c0cc0b1c469d2519
Fornax <wim@fornaxian.tech>
2023-09-26 22:47:12 +0200

    Add 18th background pattern

--- res/static/img/background_patterns/create_transparent.sh
+++ res/static/img/background_patterns/create_transparent.sh
@@ -4,7 +4,7 @@ transparent () {
 	convert $1.png -alpha set -channel a -evaluate set $2 $1_transparent.png
 }
 
-for i in {0..17}; do transparent "checker${i}" 5%; done
+for i in {0..18}; do transparent "checker${i}" 5%; done
 transparent "checker_wednesday" 8%
 transparent "checker_dwarf" 8%
 transparent "checker_developers" 8%
